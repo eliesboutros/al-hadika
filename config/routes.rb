c0cc5452fa20bb341a05d2cd49e9873b951d1a5a
Elie Boutros <elie.boutroslt@gmail.com>
2025-12-29 18:18:08 +0200

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # Set home as root page
+  root 'pages#home'
+  
+  # Define routes for other pages
+  get 'about', to: 'pages#about'
+  get 'services', to: 'pages#services'
+  get 'reviews', to: 'pages#reviews'
+  get 'contact', to: 'pages#contact'
+end
