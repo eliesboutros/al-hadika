646d351ed877c7fd82ed571dc627655455c0c09e
Elie Boutros <elie.boutroslt@gmail.com>
2026-06-08 12:35:00 +0300

    Fix route helpers in tests

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,27 +2,27 @@ require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get pages_home_url
+    get root_url
     assert_response :success
   end
 
   test "should get about" do
-    get pages_about_url
+    get about_url
     assert_response :success
   end
 
   test "should get services" do
-    get pages_services_url
+    get services_url
     assert_response :success
   end
 
   test "should get reviews" do
-    get pages_reviews_url
+    get reviews_url
     assert_response :success
   end
 
   test "should get contact" do
-    get pages_contact_url
+    get contact_url
     assert_response :success
   end
 end
